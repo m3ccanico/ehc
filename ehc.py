b03257000b2db74eeebf078b6a4321d9b2200667
Roman Ammann <m3ccanico@xxo.ch>
2016-04-15 22:25:44 +1200

    skeleton for handling chunked bodies

--- ehc.py
+++ ehc.py
@@ -5,11 +5,16 @@ import zlib
 
 end_states = (nids.NIDS_CLOSE, nids.NIDS_TIMEOUT, nids.NIDS_RESET)
 
+
+def reassemble_chunked_body(body):
+    return body
+
+
 def http_response(tcp, data):
     #print data[:20]
     ((src_ip, src_port), (dst_ip, dst_port)) = tcp.addr
     try:
-        (headers, body) = re.split(b"\r\n\r\n", data)
+        (headers, body) = re.split(b"\r\n\r\n", data, 1)
     except ValueError:
         #((src_ip, src_port), (dst_ip, dst_port)) = tcp.addr
         print "ERROR: Cannot split (ip.addr==%s and tcp.port==%s and ip.addr==%s and tcp.port==%s)" % (src_ip, src_port, dst_ip, dst_port)
@@ -17,40 +22,49 @@ def http_response(tcp, data):
         print ValueError
         print "<<<"
         return
-
+    
     # read all headers, store relevant info
     content_type = ""
     content_length = 0
     content_encoding = ""
-
+    transfer_encoding = ""
+    
     for line in headers.splitlines():
         # skip header
         if re.match("HTTP/1.1", line):
             continue
-
+        
         (name, value) = re.split(": ", line, 1)
         if name == "Content-Length":
             content_length = value
-        if name == "Content-Type":
+        elif name == "Content-Type":
             content_type = value
-        if name == "Content-Encoding":
+        elif name == "Content-Encoding":
             content_encoding = value
-
+        elif name == "Transfer-Encoding":
+            transfer_encoding = value;
+    
+    # reasemble body
+    if transfer_encoding == "chunked":
+        print "chunked"
+        body = reassemble_chunked_body(body)
+    
     # unpack compressed content
     if content_encoding == "gzip":
         try:
             body = zlib.decompress(body, zlib.MAX_WBITS|32)
+            print "decompressed (ip.addr==%s and tcp.port==%s and ip.addr==%s and tcp.port==%s)" % (src_ip, src_port, dst_ip, dst_port)
         except:
             print "ERROR: Cannot decompress (ip.addr==%s and tcp.port==%s and ip.addr==%s and tcp.port==%s)" % (src_ip, src_port, dst_ip, dst_port)
             print headers
-            print "<<<"
+            print "<<< %i" % len(body)
             return
-
+    
     #if content_type == "":
     #    print "ERROR: No content type (ip.addr==%s and tcp.port==%s and ip.addr==%s and tcp.port==%s)" % (src_ip, src_port, dst_ip, dst_port)
     #    print headers
     #    print "<<<"
-
+    
     # chose file extension based on content type
     ext = ""
     if re.search("javascript", content_type):
@@ -59,7 +73,7 @@ def http_response(tcp, data):
         ext = "html"
     elif re.search("shockwave-flash", content_type):
         ext = "swf"
-
+    
     # ignore unknonw content
     if ext != "":
         name = "%s_%s-%s_%s.%s" % (src_ip, src_port, dst_ip, dst_port, ext)
@@ -69,7 +83,6 @@ def http_response(tcp, data):
         #o.close
     else:
         print "ignored: %s" % content_type
-
 
 
 def http_stream(tcp):
@@ -84,6 +97,7 @@ def http_stream(tcp):
             end = len(tcp.client.data)
         http_response(tcp, tcp.client.data[start:end])
 
+
 def tcp_callback(tcp):
     if tcp.nids_state == nids.NIDS_JUST_EST:
         ((src_ip, src_port), (dst_ip, dst_port)) = tcp.addr
@@ -97,6 +111,7 @@ def tcp_callback(tcp):
     elif tcp.nids_state in end_states:
         http_stream(tcp)
 
+
 def main():
     nids.param("san_num_hosts", 0)          # disable portscan detections
     nids.param("filename", sys.argv[1])     # read from PCAP
@@ -104,6 +119,7 @@ def main():
     nids.init()
     nids.register_tcp(tcp_callback)
     nids.run()
+
 
 if __name__ == "__main__":
     main()
