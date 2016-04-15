7c5721c3c66b7518185555ba9da47ec2647e7f2b
Roman Ammann <m3ccanico@xxo.ch>
2016-04-16 08:13:12 +1200

    - implemented handling of chunked content - formated output

--- ehc.py
+++ ehc.py
@@ -2,16 +2,27 @@ import nids
 import sys
 import re
 import zlib
+import StringIO
+
 
 end_states = (nids.NIDS_CLOSE, nids.NIDS_TIMEOUT, nids.NIDS_RESET)
 
 
-def reassemble_chunked_body(body):
+def reassemble_chunked_body(chunked_body):
+    body = ""
+    stream = StringIO.StringIO(chunked_body)
+    while True:
+        line = stream.readline().strip()
+        if not line: break;                     # stream retruns empty string in case of EOF
+        length = int(line, 16)
+        chunk = stream.read(length)
+        body += chunk
     return body
 
 
-def http_response(tcp, data):
+def http_response(tcp, data, i):
     #print data[:20]
+    sys.stdout.write(" %i:" % i)
     ((src_ip, src_port), (dst_ip, dst_port)) = tcp.addr
     try:
         (headers, body) = re.split(b"\r\n\r\n", data, 1)
@@ -19,6 +30,7 @@ def http_response(tcp, data):
         #((src_ip, src_port), (dst_ip, dst_port)) = tcp.addr
         print "ERROR: Cannot split (ip.addr==%s and tcp.port==%s and ip.addr==%s and tcp.port==%s)" % (src_ip, src_port, dst_ip, dst_port)
         print str(data[:500])
+        #print tcp.client.data
         print ValueError
         print "<<<"
         return
@@ -46,14 +58,16 @@ def http_response(tcp, data):
     
     # reasemble body
     if transfer_encoding == "chunked":
-        print "chunked"
+        #print "chunked"
+        sys.stdout.write('c')
         body = reassemble_chunked_body(body)
     
     # unpack compressed content
     if content_encoding == "gzip":
+        sys.stdout.write('z')
         try:
             body = zlib.decompress(body, zlib.MAX_WBITS|32)
-            print "decompressed (ip.addr==%s and tcp.port==%s and ip.addr==%s and tcp.port==%s)" % (src_ip, src_port, dst_ip, dst_port)
+            #print "decompressed (ip.addr==%s and tcp.port==%s and ip.addr==%s and tcp.port==%s)" % (src_ip, src_port, dst_ip, dst_port)
         except:
             print "ERROR: Cannot decompress (ip.addr==%s and tcp.port==%s and ip.addr==%s and tcp.port==%s)" % (src_ip, src_port, dst_ip, dst_port)
             print headers
@@ -77,15 +91,18 @@ def http_response(tcp, data):
     # ignore unknonw content
     if ext != "":
         name = "%s_%s-%s_%s.%s" % (src_ip, src_port, dst_ip, dst_port, ext)
-        print "write file: %s" % name
+        sys.stdout.write(" exported %s as %s\n" % (content_type, name))
         #o = open(name, 'wb')
         #o.write(body)
         #o.close
     else:
-        print "ignored: %s" % content_type
+        sys.stdout.write(" ignored %s\n" % content_type)
+    sys.stdout.flush()
 
 
 def http_stream(tcp):
+    ((src_ip, src_port), (dst_ip, dst_port)) = tcp.addr
+    print "stream %s:%s - %s:%s" % (src_ip, src_port, dst_ip, dst_port)
     res = re.finditer(b"HTTP/1.1 2", tcp.client.data)
     lst = list(res)
 
@@ -95,7 +112,7 @@ def http_stream(tcp):
             end = lst[i+1].start()
         else:
             end = len(tcp.client.data)
-        http_response(tcp, tcp.client.data[start:end])
+        http_response(tcp, tcp.client.data[start:end], i)
 
 
 def tcp_callback(tcp):
